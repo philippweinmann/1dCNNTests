dcd92c7c6bd1befd1191fb8b596613c77c97ff8c
Philipp Weinmann <philipp.weinmann71@gmail.com>
2025-04-15 17:18:33 +0200

    something is off, lets see what

--- data.py
+++ data.py
@@ -1,27 +1,42 @@
 # %%
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
+from sklearn.datasets import make_classification
 # %%
-def generate_data(sequence_length = 500, num_features=8):
+def generate_uniform_data(sequence_length = 500, num_features=8):
     # (batch_size, sequence_length, num_features), output
     X = np.random.uniform(low = 0, high=1, size = (num_features, sequence_length)).astype(np.float32)
     y = np.array([1, 0]).astype(np.float32)
 
     return X, y
+
+def generate_proper_dataset(n_samples=105, sequence_length = 500, num_features=8):
+    # where we can test if the model actually learns something.
+    n_features = sequence_length * num_features
+    n_informative = n_features // 10
+
+    X, y = make_classification(n_samples=n_samples, n_features=n_features, n_informative=n_informative, n_redundant=n_informative, n_repeated=n_informative, n_classes=2, random_state=42)
+    X = X.reshape((n_samples, num_features, sequence_length)).astype(np.float32)
+
+    return X, y
 # %%
 class CustomDataset(Dataset):
-    def __init__(self, length):
+    def __init__(self, length, sequence_length = 500, num_features=8):
         self.length = length
+        X, y = generate_proper_dataset(n_samples=self.length)
+        self.X = X
+        self.y = y
 
     def __len__(self):
         return self.length
     
     def __getitem__(self, index):
-        X, y = generate_data()
-
-        return X, y
+        # X, y = generate_uniform_data()
+        current_X = self.X[index,:]
+        current_y = self.y[index]
+        return current_X, current_y
     
-def get_dataloader():
-    dataloader = DataLoader(CustomDataset(length=110), batch_size=4, shuffle=True)
+def get_dataloader(length):
+    dataloader = DataLoader(CustomDataset(length), batch_size=4, shuffle=True)
 
     return dataloader
--- main.py
+++ main.py
@@ -2,46 +2,66 @@
 %load_ext autoreload
 %autoreload 2
 from model import BASIC_CNN1D
-from data import get_dataloader, generate_data
+from data import get_dataloader, generate_uniform_data
 import torch
+import torch.nn.functional as F
 # %%
-dataloader = get_dataloader()
+train_dataloader = get_dataloader(length=100)
+test_dataloader = get_dataloader(length=10)
 # %%
+def test_loop(dataloader, model, loss_fn):
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            # print(f"y: {y}")
+            pred = model(X)
+            print(f"prediction: {pred}")
+            # y = F.one_hot(y, num_classes=2)
+            # y = y.to(torch.float32)
+            # print(f"y onehot: {y}")
+            test_loss += loss_fn(pred, y).item()
+            # print(f"pred: {pred}")
+            # print(f"pred argmax: {pred.argmax(1)}")
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+
+    # print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    return correct, test_loss
+
 def training_loop(dataloader, model, loss_fn, optimizer):
     model.train()
 
     for i, (X, y) in enumerate(dataloader):
-        print(X)
-        print(f"y: {y}")
+        # y = F.one_hot(y, num_classes=2)
+        # y = y.to(torch.float32)
+
         optimizer.zero_grad()
         pred = model(X)
         loss = loss_fn(pred, y)
         loss.backward()
         optimizer.step()
 
-        print(f"batch number: {i}, loss: {loss.item()}")
+        # print(f"batch number: {i}, loss: {loss.item()}")
 
 model = BASIC_CNN1D()
-epochs = 10
-loss_fn = torch.nn.BCEWithLogitsLoss()
+epochs = 100
+loss_fn = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
 
 for epoch in range(epochs):
     print(f"epoch: {epoch + 1} / {epochs}")
 
-    training_loop(dataloader=dataloader, model=model, loss_fn=loss_fn, optimizer=optimizer)
+    training_loop(dataloader=train_dataloader, model=model, loss_fn=loss_fn, optimizer=optimizer)
+    correct, test_loss = test_loop(dataloader=test_dataloader, model=model, loss_fn=loss_fn)
 
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-# %%
-# let's test it on some dummy data.
-X, y = generate_data()
-X = torch.tensor(X)
-y = torch.tensor(y)
-# %%
-pred = model(X)
-print(pred)
-# %%
-loss = loss_fn(pred, y)
-# %%
-print(loss.item())
+print("Done")
 # %%
