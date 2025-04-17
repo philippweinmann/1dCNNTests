bb2d9e3cfed68a78518e7feb2688852bde987bd3
philipp weinmann <philipp.weinmann71@gmail.com>
2025-04-17 13:40:09 +0200

    making progress

--- dummymain.py
+++ dummymain.py
@@ -36,7 +36,7 @@ class CircleDataset(Dataset):
 
 
 dataset = CircleDataset(X, y)
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+train_dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 test_dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
 
 
@@ -81,51 +81,108 @@ criterion = nn.BCEWithLogitsLoss()  # Includes sigmoid activation
 optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 
+# %%
+def is_correct(pred, y):
+    amt_correct = 0
+    for batch_num in range(pred.shape[0]):
+        current_pred = pred[batch_num]
+        current_label = y[batch_num]
+
+        # let's transform the value into percentages.
+        prob = torch.sigmoid(current_pred)
+
+        if current_label == 1 and prob > 0.5:
+            amt_correct += 1
+            continue
+
+        if current_label == 0 and prob < 0.5:
+            amt_correct += 1
+            continue
+
+    return amt_correct
+
+
+def train_loop(model, train_dataloader, loss_fn, optimizer):
+    model.train()
+
+    for inputs, labels in train_dataloader:
+        # Forward pass
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = loss_fn(outputs.squeeze(), labels)
+
+        # Backward pass and optimize
+        loss.backward()
+        optimizer.step()
+
+
 # Test loop
 def test_loop(model, test_dataloader, loss_fn):
     model.eval()
     correct = 0
-    test_loss = 0
+    total_loss = 0
 
     with torch.no_grad():
         for X, y in test_dataloader:
             pred = model(X)
-            test_loss += loss_fn(pred.squeeze(), y.squeeze()).item()
+            total_loss += loss_fn(pred.squeeze(), y.squeeze()).item()
 
-            print(pred)
-            print(y)
-
-            if pred > 0.5 and y == 1:
+            if is_correct(pred, y):
                 correct += 1
 
-            if pred < 0.5 and y == 0:
-                correct += 1
-
-    test_loss /= len(test_dataloader)
+    avg_loss = total_loss / len(test_dataloader)
     correct /= len(test_dataloader)
 
-    return test_loss, correct
+    return avg_loss, correct
 
 
 # Training loop
-num_epochs = 100
+num_epochs = 10
 
 for epoch in range(num_epochs):
-    for inputs, labels in dataloader:
-        # Forward pass
-        outputs = model(inputs)
-        print(outputs)
-        loss = criterion(outputs.squeeze(), labels)
-
-        # Backward pass and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    # test_loop
+    train_loop(model, train_dataloader, loss_fn=criterion, optimizer=optimizer)
     test_loss, accuracy = test_loop(model, test_dataloader, loss_fn=criterion)
 
     print(
         f"Epoch [{epoch + 1}/{num_epochs}], Loss: {test_loss:.4f}, Accuracy: {accuracy:.4f}"
     )
+# %%
+# let's test some of them out.
+X = np.random.uniform(low=0, high=1, size=(num_samples, 8, 400))  # 2D data points
+X[0:50, :, 0:200] = 1
+X[50:, :, 0:200] = 0
+
+X = X.astype(np.float32)
+
+y = np.ones(shape=(num_samples))
+y[0:50] = 1
+y[50:] = 0
+
+y = y.astype(np.float32)
+
+model.eval()
+
+X = torch.tensor(X)
+y = torch.tensor(y)
+
+
+def test_values(indexes):
+    for index in indexes:
+        print(f"index: {index}")
+
+        c_y = y[index]
+        c_X = X[index]
+
+        print(f"ground truth: {c_y}")
+        # print(c_X)
+
+        pred = model(c_X.unsqueeze(0))
+        print(f"pred: {pred}")
+
+        print(f"prob: {torch.sigmoid(pred)}")
+
+
+test_values(indexes=[0, 1, 2, 98, 99])
+
 # %%
